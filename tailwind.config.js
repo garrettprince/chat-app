e1e45658cb7aa237e5cd42788bd50cc4289cb21a
garrettprince <89549339+garrettprince@users.noreply.github.com>
2022-09-10 15:33:13 -0700

    Added message design, scroll finctionality, spacing fixes, all static in prep for dynamic replacement

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./lib/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./pages/**/*.{js,ts,jsx,tsx}", "./lib/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require("tailwind-scrollbar")],
 };
