39c010c263379b922f2366ef0e97a097bc4a3248
garrettprince <89549339+garrettprince@users.noreply.github.com>
2022-09-11 22:15:23 -0700

    Lots of architecture changes, introduced Forkik, need to rework component setup

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,28 @@
 import Head from "next/head";
+import { useState } from "react";
 import Feed from "../lib/components/Feed/Feed";
 import Menu from "../lib/components/Menu/Menu";
+import { supabase } from "../lib/utils/client";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import useAddUser from "../lib/hooks/useAddUser";
 
 export default function Home() {
+  const [loggedIn, setLoggedIn] = useState(false);
+  // const [addUser, isAddingUser] = useAddUser()
+  const [users, setUsers] = useState([])
+
+  const handleLoggedIn = () => {
+    setLoggedIn(!loggedIn);
+    console.log(loggedIn);
+  };
+
+  const addUser = async (user) => {
+    setUsers([...users, user])
+    await supabase.from("users").insert(user);
+
+    console.log(user)
+  };
+
   return (
     <div>
       <Head>
@@ -11,9 +31,64 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="flex justify-center h-screen py-12">
+      {!loggedIn && (
+        <div className="absolute w-full h-full bg-white flex flex-col justify-center items-center">
+          <div className="text-4xl font-bold mb-5">
+            Welcome to{" "}
+            <span className="bg-green-500 text-white py-2 px-3 rounded-xl">
+              Chat App!
+            </span>
+          </div>
+          <p className="text-xl mb-8">Please create an account below</p>
+          <Formik
+            initialValues={{ username: "", password: "", profilePhotoUrl: "" }}
+            onSubmit={async (values) => {
+              // submit to database
+              addUser(values);
+              handleLoggedIn();
+            }}
+          >
+            {({ values, dirty, setFieldValue }) => (
+              <Form className="flex flex-col">
+                <p className="text-gray-500 ml-2 text-sm mb-1">Username</p>
+                <Field
+                  type="text"
+                  name="username"
+                  className="border border-gray-500 w-80 rounded-xl p-2 mb-4"
+                />
+                <p className="text-gray-500 ml-2 text-sm mb-1">Password</p>
+                <Field
+                  type="password"
+                  name="password"
+                  className="border border-gray-500 w-80 rounded-xl p-2 mb-4"
+                />
+                <p className="text-gray-500 ml-2 text-sm mb-1">
+                  Profile Photo URL
+                </p>
+                <Field
+                  type="text"
+                  name="profilePhotoUrl"
+                  className="border border-gray-500 w-80 rounded-xl p-2 mb-8"
+                />
+                <div className="flex justify-around">
+                  <button
+                    type="submit"
+                    className="text-white font-bold bg-black px-4 py-2 rounded-lg"
+                  >
+                    Create
+                  </button>
+                  <button type="submit" className="">
+                    Log In
+                  </button>
+                </div>
+              </Form>
+            )}
+          </Formik>
+        </div>
+      )}
 
-        <Menu />
+      <main className="flex justify-center h-screen py-12">
+        <Menu handleLoggedIn={handleLoggedIn} />
 
         <Feed />
       </main>
